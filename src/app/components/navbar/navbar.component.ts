01bcc47911342369a560d7245331291218abdc18
Ghislain Kean O. David <112994121+GhislainKeanDavid@users.noreply.github.com>
2024-12-18 16:31:05 +0800

    Added contents
    Added content for the landing page
    updated the bootstrap
    gago si reg

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,48 @@
-import { Component } from '@angular/core';
-import { Router } from '@angular/router';
+// import { Component } from '@angular/core';
+// import { Router } from '@angular/router';
+
+// @Component({
+//   selector: 'app-navbar',
+//   templateUrl: './navbar.component.html',
+//   styleUrls: ['./navbar.component.css']
+// })
+// export class NavbarComponent {
+//   constructor(private router: Router) {}
+
+//   navigateToSignup(): void {
+//     this.router.navigate(['/signup']);
+//   }
+// }
+
+import { Component, OnInit } from '@angular/core';
+import { AuthService } from '../../services/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.css']
 })
-export class NavbarComponent {
-  constructor(private router: Router) {}
+export class NavbarComponent implements OnInit {
+  isLoggedIn: boolean = false;
+  private userSub!: Subscription;
 
-  navigateToSignup(): void {
-    this.router.navigate(['/signup']);
+  constructor(private authService: AuthService) {}
+
+  ngOnInit() {
+    this.userSub = this.authService.userSub.subscribe(user => {
+      this.isLoggedIn = !!user; // Convert the user object to a boolean
+    });
+  }
+
+  onLogout() {
+    this.authService.logout();
+  }
+
+  ngOnDestroy() {
+    if (this.userSub) {
+      this.userSub.unsubscribe();
+    }
   }
 }
+
